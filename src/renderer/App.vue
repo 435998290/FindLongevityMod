015971e93991f89bcbe82d14b8d822899c887f16
swy <435998290@qq.com>
2022-02-11 02:10:49 +0800

    完成electron+vue+elmentUI配置

---
+++ src/renderer/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="window">
+    <div  class="header">觅长生修改工具</div>
+    <div class="container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'find-longevity-mod'
+}
+</script>
+
+<style>
+/* CSS */
+/* CSS */
+@font-face {
+    font-family: dragon;
+    src: url("/static/font/dragon.ttf");
+  }
+.window {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-image: url("/static/imgs/background.png");
+  background-size: 1000px 563px;
+  height: 100vh;
+}
+.header {
+  font-family: "dragon";
+  font-size: 50px;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  height: 394px;
+  background-color: white;
+}
+</style>
